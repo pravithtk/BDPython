3a8799e9f574118742e5fe4555ff0305734aa6ee
pravithtk <praviith@gmail.com>
2024-02-07 11:59:54 +0000

    py

--- py_practise.py
+++ py_practise.py
@@ -27,3 +27,20 @@
 # print("enter num2 value after:",y)
 
 #-------------------------
+#prime number
+
+# num=6
+# count=0
+
+# if num>1:
+#     for i in range(1,num+1):
+#             if(num%i) == 0:
+#                 count=count+1
+#     if count ==2:
+#             print("its prime number")
+#     else:
+#             print("it's not a prime number")
+
+#----------------------------------------------------------
+
+
