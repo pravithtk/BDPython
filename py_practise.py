3789cc767befcc9321cd3ae0cdbf9dfdd97d2b84
pravithtk <praviith@gmail.com>
2024-02-07 10:48:01 +0000

    ptc

---
+++ py_practise.py
@@ -0,0 +1,29 @@
+#1swapping values
+# x=10
+# y=20
+
+# print("enter num1 value before:",x)
+# print("enter num2 value before:",y)
+
+# temp=x
+# x=y
+# y=temp
+
+# print("enter num1 value after:",x)
+# print("enter num2 value after:",y)
+#------------------------------------------------
+# x=input("enter num1:")
+# y=input("enter num2:")
+
+#temp varialbe method
+# temp=x
+# x=y
+# y=temp
+
+#direct method
+# x,y=y,x
+
+# print("enter num1 value after:",x)
+# print("enter num2 value after:",y)
+
+#-------------------------
