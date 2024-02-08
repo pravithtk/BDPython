e36bd87b3e074e29f7ae7d139c38cf373b627d22
pravithtk <praviith@gmail.com>
2024-02-08 12:37:35 +0000

    ihhi

--- py_practise.py
+++ py_practise.py
@@ -42,5 +42,24 @@
 #             print("it's not a prime number")
 
 #----------------------------------------------------------
+#prime
+# num =input("input number prime or not:")
+# count=0
+
+# if num>1:
+#     for i in range(1,num+1):
+#             if(num%i) == 0:
+#                 count=count+1
+#     if count ==2:
+#             print("its prime number:")
+#     else:
+        
+#             print("it's not a prime number")
+#-----------------------------------------------------
+
+
+
+            
+
 
 
